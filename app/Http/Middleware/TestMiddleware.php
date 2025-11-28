30e2dbcb16b983a575cdab835da375fd8c4c40af
Kawthar Shaalan <101220311@mu.edu.lb>
2025-11-28 13:52:27 +0200

    added some folders and set the connection

---
+++ app/Http/Middleware/TestMiddleware.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class TestMiddleware
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
